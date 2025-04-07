c76c6e65ea96efd6d424aa1b4467e8dd9a4ea90e
柳清爽 <eison.icu@gmail.com>
2025-04-07 15:09:07 +0800

    新建获客计划 - 设备选择

--- Server/application/plan/route.php
+++ Server/application/plan/route.php
@@ -19,11 +19,6 @@ Route::group('api/plan', function () {
     Route::get('traffic/stats', 'plan/Traffic/sourceStats');
     Route::post('traffic/import', 'plan/Traffic/importTraffic');
     Route::post('traffic/external', 'plan/Traffic/handleExternalTraffic');
-    
-    // 标签相关路由
-    Route::resource('tags', 'plan/Tag');
-    Route::post('tags/batch', 'plan/Tag/batchCreate');
-    Route::get('tags/names', 'plan/Tag/getNames');
 });
 
 // 返回空数组，避免路由注册冲突
